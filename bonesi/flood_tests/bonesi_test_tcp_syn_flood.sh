e4735e8c6e5ef52d0f9d8b98f9fba3e4bfc10b53
arthurguimaraesferreira <arthuragf@outlook.com.br>
2025-11-10 16:45:08 -0300

    BoNeSi TCP Flood Test Script Correction

--- bonesi/flood_tests/bonesi_test_tcp_syn_flood.sh
+++ bonesi/flood_tests/bonesi_test_tcp_syn_flood.sh
@@ -5,6 +5,6 @@ OUTPUT_FILE="bonesi_output.txt"
 # BoNeSi TCP SYN Flood (60 seconds)
 sudo timeout -s INT 60 bonesi -i bots.txt -p tcp -s 0 -d enp0s3 192.168.100.2:50001 > "$OUTPUT_FILE" 2>&1
 
-TOTAL_PACKETS=$(grep -oP '\d+(?= packets in)' "$OUTPUT_FILE" | awk '{s+=$1} END {print s}')
+TOTAL_PACKETS=$(grep -oP '\d+(?= requests in)' "$OUTPUT_FILE" | awk '{s+=$1} END {print s}')
 
 echo "$TOTAL_PACKETS packets sent"
